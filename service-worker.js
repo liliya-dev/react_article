0f0b2f6287188cb1df95c029163bfc66227a8d37
Liliia <chuiko1112@gmail.com>
2020-06-16 22:27:04 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.575c894ccaaaec018efa3e6d1c5de3fa.js"
+  "./precache-manifest.92ab398deff43a70323fd8cabaa05e9c.js"
 );
 
 self.addEventListener('message', (event) => {
